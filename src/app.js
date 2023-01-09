a718bea289f3e9592390c7d68080929a5786baea
Jonathan <joni13_escorpio@gmail.com>
2023-01-09 05:46:29 -0300

    add backend user

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const db = require("./utils/database");
 const initModels = require("./models/init.model");
+const Users = require("./models/users.model");
 
 // crear instancia express
 const app = express();
-
+app.use(express.json());
 const PORT = 8000;
 
 //probando la conexion a sequalize
@@ -23,6 +24,66 @@ db.sync({ alter: true })
 app.get("/", (req, res) => {
   // lo mismo se puede poner por separado
   res.status(200).json({ message: "Bienvenido al servidor" });
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const result = await Users.findAll();
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    console.log(req.params);
+    const { id } = req.params;
+    const result = await Users.findByPk(id);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/user/username/:username", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const result = await Users.findOne({ where: { username } });
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/users", async (req, res) => {
+  try {
+    const user = req.body;
+    const result = await Users.create(user);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json(error.message);
+    console.log(error);
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  try {
+    const field = req.body;
+    const { id } = req.params;
+    const result = await Users.update(field, { where: { id } });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Users.destroy({ where: { id } });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 });
 
 app.listen(PORT, () => {

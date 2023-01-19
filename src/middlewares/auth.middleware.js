8bef68883f142587794830b96df217c7c145cae9
Jonathan <joni13_escorpio@gmail.com>
2023-01-18 22:16:32 -0300

    option token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,24 +3,31 @@ require("dotenv").config();
 
 const authMiddleware = (req, res, next) => {
   let { authorization: token } = req.headers;
-  token = token.replace("Bearer ", "");
+  token = token?.replace("Bearer ", "");
   console.log(token);
-  jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    { algorithms: "HS512" },
-    (err, decoded) => {
-      if (err) {
-        res.status(400).json({
-          error: "invalid token",
-          message: "el token no es valido, envia uno correcto",
-        });
-      } else {
-        console.log(decoded);
-        next();
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { algorithms: "HS512" },
+      (err, decoded) => {
+        if (err) {
+          res.status(400).json({
+            error: "invalid token",
+            message: "el token no es valido, envia uno correcto",
+          });
+        } else {
+          console.log(decoded);
+          next();
+        }
       }
-    }
-  );
+    );
+  } else {
+    res.status(400).json({
+      error: "invalid token",
+      message: "no se esta enviando un token de autenticacion",
+    });
+  }
 };
 
 module.exports = authMiddleware;
